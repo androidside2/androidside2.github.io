e87fd9f320b621a9434803a75e8ba6ae8520ea9b
androidside2@Sauron <androidside2>
2017-09-16 17:25:21 -0400

    first commit

---
+++ assets/include/php/example.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__.'/include/WakeOnLAN.php';
+\WakeOnLAN::wakeUp('E8:D1:1B:5D:4F:AE', '108.18.33.200');
+
+?>
---
+++ assets/include/php/example2.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+if($_POST['action'] == 'call_this') {
+  echo "Hello World!";
+}
+
+?>
+
+</body>
+</html>
